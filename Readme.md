8282285c0b335f11d0869a21fac4dce36926b302
Jovito Isorena Jr <jovito.isorena@GXCHLAP00705.local>
2025-07-01 14:47:25 +0800

    Final Submission, FECP-Java-Session1-Operators

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Project Title 
+FECP-Java-Session1-Operators
+# Screenshot of your output
+![image info](./lab1.png)
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,22 @@ import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        /* Write a program that takes in a float Celsius, and displays
+        its equivalent value in Fahrenheit in two decimal places. The
+        formula is as follows: °F = °C × (9/5) + 32.
+         */
+
+        Scanner sc = new Scanner(System.in);
+        float input, output;
+        try {
+            System.out.print("Enter temperature in Celsius: ");
+            input = sc.nextFloat();
+            output = input * 9/5 + 32;
+            System.out.printf("Temperature in Fahrenheit: %.2f%n", output);
+
+        }catch (NumberFormatException e){
+
+        }
 
     }
 }
